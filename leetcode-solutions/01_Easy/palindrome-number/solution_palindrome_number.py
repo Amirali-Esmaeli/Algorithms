4de4007095f76f9d8da6294b46d14dccf08d493a
Amirali <amiraliesmaeli741@gmail.com>
2025-07-09 20:28:44 +0330

    Add Palindrome Number

---
+++ leetcode-solutions/01_Easy/palindrome-number/solution_palindrome_number.py
@@ -0,0 +1,19 @@
+# Problem: Palindrome Number
+# Difficulty: Easy
+
+# Input: x = 121
+# Output: true
+
+# Solution – String Reverse O(log₁₀(x))
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        
+        original = x
+        reverse = 0
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x //= 10
+        return original == reverse
